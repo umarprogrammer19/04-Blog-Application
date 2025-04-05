915ee25c4ef3dc704d448610950565b2800f7afb
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-05 22:27:59 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,7 @@ export async function middleware(req: NextRequest) {
     }
 }
 
+// For Setting The Routes 
 export const config = {
     matcher: ['/', '/dashboard', '/about', '/blogs', '/contact'],
 };
